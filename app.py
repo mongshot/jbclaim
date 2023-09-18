2f41c3a642a73535169291de8758e9a2c7f222ca
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-18 11:40:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap  # Bootstrap 추가
+from flask_bootstrap import Bootstrap
 from telegram import Bot
 import asyncio
 import os
 
 app = Flask(__name__)
 app.debug = True
-Bootstrap(app)  # Bootstrap 초기화
+Bootstrap(app)
+
+# 환경 변수에서 토큰과 채팅 ID를 가져옵니다.
+TELEGRAM_BOT_TOKEN = '6560335312:AAHo82hdFJr1q_6CKUkms7NkL68kwgMul08'
+TELEGRAM_CHAT_ID = '71046013'
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)  # 봇 객체 초기화
 
 async def send_telegram_message(message):
     await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-
-# 나머지 코드는 그대로 유지됩니다.
 
 @app.route('/')
 def home():
@@ -32,11 +36,9 @@ def submit():
         loop.run_until_complete(send_telegram_message(message))
     except Exception as e:
         error_message = f'오류 발생: {str(e)}'
-        return render_template('error.html', error_message=error_message)  # 오류 템플릿으로 리다이렉트
+        return render_template('error.html', error_message=error_message)
 
-    return render_template('success.html')  # 성공 템플릿으로 리다이렉트
-
-# 나머지 코드는 그대로 유지됩니다.
+    return render_template('success.html')
 
 if __name__ == '__main__':
     try:
