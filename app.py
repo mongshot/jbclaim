99a516b022168d3041f715a1d7664f6865165162
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-18 13:53:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,38 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from telegram import Bot
-import asyncio
-import os  # 환경 변수 사용을 위해 os 모듈을 가져옵니다.
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 app = Flask(__name__)
-app.debug = True
-app.secret_key = 'snoopy'  # 시크릿 키를 설정합니다.
+app.secret_key = 'your_secret_key'  # 보안을 위한 시크릿 키
 
-# 환경 변수에서 토큰과 채팅 ID를 가져옵니다.
-TELEGRAM_BOT_TOKEN = '6560335312:AAHo82hdFJr1q_6CKUkms7NkL68kwgMul08'
-TELEGRAM_CHAT_ID = '71046013'
+# 간단한 차량 데이터베이스
+cars = [
+    {'id': 1, 'name': 'Car 1', 'available': True},
+    {'id': 2, 'name': 'Car 2', 'available': True},
+    {'id': 3, 'name': 'Car 3', 'available': True},
+    {'id': 4, 'name': 'Car 4', 'available': True},
+    {'id': 5, 'name': 'Car 5', 'available': True},
+    {'id': 6, 'name': 'Car 6', 'available': True},
+    {'id': 7, 'name': 'Car 7', 'available': True}
+]
 
-# 환경 변수가 설정되어 있지 않으면 기본 값을 사용합니다.
-if not TELEGRAM_BOT_TOKEN:
-    TELEGRAM_BOT_TOKEN = 'YOUR_DEFAULT_BOT_TOKEN'
-
-if not TELEGRAM_CHAT_ID:
-    TELEGRAM_CHAT_ID = 'YOUR_DEFAULT_CHAT_ID'
-
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
+# 간단한 예약 데이터베이스
+reservations = []
 
 @app.route('/')
 def home():
-    return render_template('report.html')
+    return render_template('index.html', cars=cars)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    try:
-        reporter = request.form['reporterName']
-        location = request.form['reportLocation']
-        complaint = request.form['complaint']
-
-        message = f'신고인: {reporter}\n신고위치: {location}\n신고사항: {complaint}'
-
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        loop.run_until_complete(send_telegram_message(message))
-        
-        flash('신고가 접수되었습니다. 감사합니다!', 'success')
-        return redirect(url_for('home'))
-    except Exception as e:
-        flash(f'오류 발생: {str(e)}', 'error')
-        return redirect(url_for('home'))
-
-async def send_telegram_message(message):
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+@app.route('/reserve/<int:car_id>/<time_slot>')
+def reserve(car_id, time_slot):
+    car = next((c for c in cars if c['id'] == car_id), None)
+    if car:
+        if car['available']:
+            car['available'] = False
+            reservations.append({'car_id': car_id, 'time_slot': time_slot, 'timestamp': datetime.now()})
+            flash('예약이 완료되었습니다.', 'success')
+        else:
+            flash('이미 예약된 차량입니다.', 'danger')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    try:
-        app.run()
-    except Exception as e:
-        print('An error occurred:', str(e))
+    app.run(debug=True)
