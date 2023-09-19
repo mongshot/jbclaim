072b25d7eb08f6cadaac7f1168dd3a4f30c22163
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-19 14:03:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+
+app = Flask(__name__)
 
 # CSV 파일에서 데이터 읽기
 def load_data(file_path):
@@ -19,25 +22,26 @@ def add_vehicle(data, vehicle_id, make, model, year, available):
 def save_data(data, file_path):
     data.to_csv(file_path, index=False)
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
     csv_file = "vehicles.csv"
     vehicle_data = load_data(csv_file)
+    return render_template('index.html', vehicle_data=vehicle_data)
 
-    while True:
-        print("1. 차량 추가")
-        print("2. 종료")
-        choice = input("선택: ")
+@app.route('/add_vehicle', methods=['POST'])
+def add_vehicle_route():
+    vehicle_id = int(request.form['vehicle_id'])
+    make = request.form['make']
+    model = request.form['model']
+    year = int(request.form['year'])
+    available = request.form['available']
 
-        if choice == "1":
-            vehicle_id = int(input("차량 ID: "))
-            make = input("제조사: ")
-            model = input("모델: ")
-            year = int(input("연식: "))
-            available = input("사용 가능 여부 (Yes/No): ")
+    csv_file = "vehicles.csv"
+    vehicle_data = load_data(csv_file)
+    vehicle_data = add_vehicle(vehicle_data, vehicle_id, make, model, year, available)
+    save_data(vehicle_data, csv_file)
 
-            vehicle_data = add_vehicle(vehicle_data, vehicle_id, make, model, year, available)
-            save_data(vehicle_data, csv_file)
-            print("차량이 추가되었습니다.")
-        elif choice == "2":
-            break
+    return redirect(url_for('index'))
 
+if __name__ == "__main__":
+    app.run(debug=True)
