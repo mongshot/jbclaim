afe5f52b4518efc24c24d939a59f3eebda3b15a1
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-19 14:10:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,12 +22,32 @@ def add_vehicle(data, vehicle_id, make, model, year, available):
 def save_data(data, file_path):
     data.to_csv(file_path, index=False)
 
+# 차량 예약
+@app.route('/reserve', methods=['POST'])
+def reserve():
+    vehicle_id = int(request.form['vehicle_id'])
+
+    # 차량 데이터를 불러오고 예약 상태를 변경합니다.
+    csv_file = "vehicles.csv"
+    vehicle_data = load_data(csv_file)
+    
+    # 예약 가능한 차량인지 확인합니다.
+    row = vehicle_data.loc[vehicle_data['vehicle_id'] == vehicle_id].iloc[0]
+    if row['available'] == 'Yes':
+        vehicle_data.loc[vehicle_data['vehicle_id'] == vehicle_id, 'available'] = 'No'
+        save_data(vehicle_data, csv_file)
+        return redirect(url_for('index'))
+    else:
+        return "이 차량은 이미 예약되었습니다."
+
+# 웹 페이지 렌더링
 @app.route('/')
 def index():
     csv_file = "vehicles.csv"
     vehicle_data = load_data(csv_file)
     return render_template('index.html', vehicle_data=vehicle_data)
 
+# 차량 데이터 추가
 @app.route('/add_vehicle', methods=['POST'])
 def add_vehicle_route():
     vehicle_id = int(request.form['vehicle_id'])
