6247c66316a24fc782b811ac6b01d7d809575e94
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-19 14:20:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import datetime
 
 app = Flask(__name__)
 
-# CSV 파일에서 데이터 읽기
+# CSV 파일에서 차량 및 예약 데이터 읽기
 def load_data(file_path):
     try:
         data = pd.read_csv(file_path)
@@ -12,56 +13,54 @@ def load_data(file_path):
         print(f"File {file_path} not found. Creating an empty DataFrame.")
         return pd.DataFrame(columns=['vehicle_id', 'make', 'model', 'year', 'available'])
 
-# 차량 데이터 추가
-def add_vehicle(data, vehicle_id, make, model, year, available):
-    new_row = {'vehicle_id': vehicle_id, 'make': make, 'model': model, 'year': year, 'available': available}
-    data = data.append(new_row, ignore_index=True)
-    return data
+def load_reservations(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        data['start_time'] = pd.to_datetime(data['start_time'])
+        data['end_time'] = pd.to_datetime(data['end_time'])
+        return data
+    except FileNotFoundError:
+        print(f"File {file_path} not found. Creating an empty DataFrame.")
+        return pd.DataFrame(columns=['reservation_id', 'vehicle_id', 'user_id', 'start_time', 'end_time'])
 
-# 차량 데이터 저장
+# 차량 및 예약 데이터 저장
 def save_data(data, file_path):
     data.to_csv(file_path, index=False)
 
-# 차량 예약
+# 예약 기능
 @app.route('/reserve', methods=['POST'])
 def reserve():
     vehicle_id = int(request.form['vehicle_id'])
+    user_id = request.form['user_id']
+    start_time = datetime.datetime.strptime(request.form['start_time'], '%Y-%m-%d %H:%M')
+    end_time = datetime.datetime.strptime(request.form['end_time'], '%Y-%m-%d %H:%M')
 
-    # 차량 데이터를 불러오고 예약 상태를 변경합니다.
-    csv_file = "vehicles.csv"
-    vehicle_data = load_data(csv_file)
-    
-    # 예약 가능한 차량인지 확인합니다.
-    row = vehicle_data.loc[vehicle_data['vehicle_id'] == vehicle_id].iloc[0]
-    if row['available'] == 'Yes':
-        vehicle_data.loc[vehicle_data['vehicle_id'] == vehicle_id, 'available'] = 'No'
-        save_data(vehicle_data, csv_file)
-        return redirect(url_for('index'))
-    else:
-        return "이 차량은 이미 예약되었습니다."
+    reservations = load_reservations('reservations.csv')
 
-# 웹 페이지 렌더링
-@app.route('/')
-def index():
-    csv_file = "vehicles.csv"
-    vehicle_data = load_data(csv_file)
-    return render_template('index.html', vehicle_data=vehicle_data)
+    # 예약이 가능한지 확인
+    for index, row in reservations.iterrows():
+        if row['vehicle_id'] == vehicle_id:
+            if not (end_time <= row['start_time'] or start_time >= row['end_time']):
+                return "이미 예약된 시간입니다."
 
-# 차량 데이터 추가
-@app.route('/add_vehicle', methods=['POST'])
-def add_vehicle_route():
-    vehicle_id = int(request.form['vehicle_id'])
-    make = request.form['make']
-    model = request.form['model']
-    year = int(request.form['year'])
-    available = request.form['available']
+    reservation_id = len(reservations) + 1
+    new_reservation = {
+        'reservation_id': reservation_id,
+        'vehicle_id': vehicle_id,
+        'user_id': user_id,
+        'start_time': start_time,
+        'end_time': end_time
+    }
 
-    csv_file = "vehicles.csv"
-    vehicle_data = load_data(csv_file)
-    vehicle_data = add_vehicle(vehicle_data, vehicle_id, make, model, year, available)
-    save_data(vehicle_data, csv_file)
+    reservations = reservations.append(new_reservation, ignore_index=True)
+    save_data(reservations, 'reservations.csv')
+    return "예약이 완료되었습니다."
 
-    return redirect(url_for('index'))
+# 예약 목록 표시
+@app.route('/reservations')
+def show_reservations():
+    reservations = load_reservations('reservations.csv')
+    return render_template('reservations.html', reservations=reservations)
 
 if __name__ == "__main__":
     app.run(debug=True)
