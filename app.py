55444763f7ae9de6c30bca0df64c75bcb0109d4d
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-19 10:21:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app.secret_key = 'your_secret_key'  # 보안을 위한 시크릿 키
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'  # SQLite 데이터베이스 설정
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-db.create_all()
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,58 +24,63 @@ class Reservation(db.Model):
 
 @app.route('/')
 def home():
-    cars = Car.query.filter_by(available=True).all()
+    with app.app_context():
+        cars = Car.query.filter_by(available=True).all()
     return render_template('index.html', cars=cars)
 
 @app.route('/admin')
 def admin():
-    cars = Car.query.all()
+    with app.app_context():
+        cars = Car.query.all()
     return render_template('admin.html', cars=cars)
 
 @app.route('/admin/update/<int:car_id>', methods=['POST'])
 def update_car(car_id):
-    car = Car.query.get_or_404(car_id)
+    with app.app_context():
+        car = Car.query.get_or_404(car_id)
 
-    # 차량 번호를 숫자로 업데이트
-    car.id = int(request.form['new_car_id'])
-    car.name = request.form['new_car_name']
-    db.session.commit()
+        # 차량 번호를 숫자로 업데이트
+        car.id = int(request.form['new_car_id'])
+        car.name = request.form['new_car_name']
+        db.session.commit()
 
     flash(f'차량 정보가 업데이트되었습니다. (ID: {car.id})', 'success')
     return redirect(url_for('admin'))
 
 @app.route('/cancel_reservation/<int:reservation_id>')
 def cancel_reservation(reservation_id):
-    reservation = Reservation.query.get_or_404(reservation_id)
-    car = reservation.car
-    car.available = True
-    db.session.delete(reservation)
-    db.session.commit()
+    with app.app_context():
+        reservation = Reservation.query.get_or_404(reservation_id)
+        car = reservation.car
+        car.available = True
+        db.session.delete(reservation)
+        db.session.commit()
 
     flash('예약이 취소되었습니다.', 'success')
     return redirect(url_for('home'))
 
 @app.route('/reserve/<int:car_id>/<time_slot>', methods=['POST'])
 def reserve(car_id, time_slot):
-    car = Car.query.get_or_404(car_id)
+    with app.app_context():
+        car = Car.query.get_or_404(car_id)
 
-    if car.available:
-        reservation_name = request.form.get('reservation_name', '')
+        if car.available:
+            reservation_name = request.form.get('reservation_name', '')
 
-        if reservation_name:
-            car.available = False
-            car.reservation_id = len(Reservation.query.all()) + 1
-            car.reservation_name = reservation_name
+            if reservation_name:
+                car.available = False
+                car.reservation_id = len(Reservation.query.all()) + 1
+                car.reservation_name = reservation_name
 
-            reservation = Reservation(car_id=car.id, time_slot=time_slot)
-            db.session.add(reservation)
-            db.session.commit()
+                reservation = Reservation(car_id=car.id, time_slot=time_slot)
+                db.session.add(reservation)
+                db.session.commit()
 
-            flash(f'{reservation_name} 님의 예약이 완료되었습니다.', 'success')
+                flash(f'{reservation_name} 님의 예약이 완료되었습니다.', 'success')
+            else:
+                flash('예약자명을 입력하세요.', 'danger')
+                return redirect(url_for('reserve', car_id=car_id, time_slot=time_slot))
         else:
-            flash('예약자명을 입력하세요.', 'danger')
-            return redirect(url_for('reserve', car_id=car_id, time_slot=time_slot))
-    else:
-        flash('이미 예약된 차량입니다.', 'danger')
+            flash('이미 예약된 차량입니다.', 'danger')
 
     return
